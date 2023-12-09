a19893ebf38c8422189d4d7a4332d67fd5dbaceb
JustWantCookies <144944802+JustWantCookies@users.noreply.github.com>
2023-12-09 17:03:52 +0100

    init board comment

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -8,6 +8,8 @@ public class Board {
 
     public Board(){
         cells = new char[3][3];
+
+        // Init board
         for(int i =0; i <3; i++){
             for(int j =0; j <3; j++){
                 cells[i][j] = ' ';

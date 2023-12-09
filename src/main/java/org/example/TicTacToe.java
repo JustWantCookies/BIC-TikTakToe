0ba6b3342991d9002b70261f30fdfb0b15e036ad
Torsten Egert <torsten.egert@martinek.or.at>
2023-12-09 17:52:43 +0100

    modfication of member-initialization get row, col from console in method start

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,25 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class TicTacToe
 {
     private Player player1;
     private Player player2;
-
     private Player currentPlayer;
+    private  Board board;
 
     public TicTacToe()
     {
-
+        board=new Board();
+        player1=new Player('X');
+        player2=new Player('0');
     }
 
     public void start()
     {
+        Scanner scanner=new Scanner(System.in);
+
+        switchCurrentPlayer();
+        board.print();
+
+        System.out.print("row(0,2);");
+        int row=scanner.nextInt();
+        System.out.print("column(0,2):");
+        int col=scanner.nextInt();
 
     }
 
     public void switchCurrentPlayer()
     {
+        if(currentPlayer.equals(player1))
+            currentPlayer=player2;
+        else
+            currentPlayer=player1;
 
+        System.out.printf("Current Player: %s",currentPlayer);
     }
 
     public boolean hasWinner()

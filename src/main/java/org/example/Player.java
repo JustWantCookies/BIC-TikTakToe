7bc422e372b2c4e3576418212712165390d2232c
Torsten Egert <torsten.egert@martinek.or.at>
2023-12-09 16:46:43 +0100

    added class Player

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Player
+{
+    char marker;
+
+    public Player(char marker)
+    {
+        this.marker = marker;
+    }
+
+    public char getMarker()
+    {
+        return marker;
+    }
+}

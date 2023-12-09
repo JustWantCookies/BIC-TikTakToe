b2486fde607fa9332bb8f11d8a5fe8efe16adc68
Torsten Egert <torsten.egert@martinek.or.at>
2023-12-09 18:10:17 +0100

    TicTacToeTest add skeletions for switchCurrentPlayer-Test

---
+++ src/test/java/org/example/TicTacToeTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TicTacToeTest
+{
+    @Test
+    public void switchCurrentPlayerTestTrue() {
+        TicTacToe ticTacToe=new TicTacToe();
+
+    }
+
+    @Test
+    public void switchCurrentPlayerTestFalse() {
+        TicTacToe ticTacToe=new TicTacToe();
+    }
+}

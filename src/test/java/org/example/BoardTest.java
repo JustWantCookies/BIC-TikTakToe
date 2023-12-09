1817b1ea95bed436b48eddb39c915990f087e6b4
JustWantCookies <144944802+JustWantCookies@users.noreply.github.com>
2023-12-09 17:16:08 +0100

    add isFull test function

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -8,51 +8,71 @@ import static org.junit.Assert.assertTrue;
 public class BoardTest {
 
     @Test
-    public void isEmptyTrueTest(){
+    public void isEmptyTrueTest() {
         Board board = new Board();
-        boolean res = board.isCellEmpty(0,0);
+        boolean res = board.isCellEmpty(0, 0);
         assertTrue(res);
     }
 
     @Test
-    public void isEmptyFalseTest(){
+    public void isEmptyFalseTest() {
         Board board = new Board();
-        board.place(0,0, 'X');
-        boolean res = board.isCellEmpty(0,0);
+        board.place(0, 0, 'X');
+        boolean res = board.isCellEmpty(0, 0);
         assertFalse(res);
     }
 
     @Test
-    public void placeTestZeroZero(){
+    public void placeTestZeroZero() {
         Board board = new Board();
-        board.place(0,0, 'X');
-        boolean res = board.isCellEmpty(0,0);
+        board.place(0, 0, 'X');
+        boolean res = board.isCellEmpty(0, 0);
         assertFalse(res);
     }
 
     @Test
-    public void placeTestOneOne(){
+    public void placeTestOneOne() {
         Board board = new Board();
-        board.place(1,1, 'X');
-        boolean res = board.isCellEmpty(1,1);
+        board.place(1, 1, 'X');
+        boolean res = board.isCellEmpty(1, 1);
         assertFalse(res);
     }
 
     @Test
-    public void clearBoardTest(){
+    public void clearBoardTest() {
         Board board = new Board();
         board.clear();
-        boolean res = board.isCellEmpty(0,0);
+        boolean res = board.isCellEmpty(0, 0);
         assertTrue(res);
     }
 
     @Test
-    public void clearPlaceBoardTest(){
+    public void clearPlaceBoardTest() {
         Board board = new Board();
-        board.place(0,0, 'X');
-        board.place(2,1, 'X');
+        board.place(0, 0, 'X');
+        board.place(2, 1, 'X');
         board.clear();
-        boolean res = board.isCellEmpty(0,0) && board.isCellEmpty(2,1);
+        boolean res = board.isCellEmpty(0, 0) && board.isCellEmpty(2, 1);
         assertTrue(res);
+    }
+
+    @Test
+    public void isFullTrueTest() {
+        Board board = new Board();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board.place(i, j, 'X');
+            }
+        }
+
+        assertTrue(board.isFull());
+    }
+
+    @Test
+    public void isFullFalseTest() {
+        Board board = new Board();
+
+        assertFalse(board.isFull());
     }
 }

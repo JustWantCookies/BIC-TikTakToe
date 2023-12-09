5a7269e64f76a73a2baa529c20119acd05987f45
Torsten Egert <torsten.egert@martinek.or.at>
2023-12-09 17:36:20 +0100

    TicTacToe: create method skeletions

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -2,4 +2,28 @@ package org.example;
 
 public class TicTacToe
 {
+    private Player player1;
+    private Player player2;
+
+    private Player currentPlayer;
+
+    public TicTacToe()
+    {
+
+    }
+
+    public void start()
+    {
+
+    }
+
+    public void switchCurrentPlayer()
+    {
+
+    }
+
+    public boolean hasWinner()
+    {
+        return false;
+    }
 }
